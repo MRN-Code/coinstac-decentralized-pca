258827227affd8aeb62f96c2895f9f73813d13ec
Rogers F Silva <rsilva@mrn.org>
2018-10-03 21:50:46 -0600

    Included docstring for local_1()

--- local.py
+++ local.py
@@ -15,6 +15,56 @@ import local_ancillary as la
 
 
 def local_1(args):
+    """ Parse input data and parameters, trigger computation of the local PCA, send results to remote
+    
+    Parameters
+    ----------
+    args : dict
+        Example:
+        {
+        "input": {
+            "data": [
+                [
+                    "local0.sub0.data.npz",
+                    "local0.sub1.data.npz",
+                    "local0.sub2.data.npz",
+                    "local0.sub3.data.npz",
+                    "local0.sub4.data.npz"
+                ],
+                [
+                    "npzfile"
+                ]
+            ],
+            "num_PC_global": 20,
+            "axis": -1,
+            "mean_values": [
+                [
+                    "row_mean_global.npz"
+                ],
+                [
+                    "npzfile"
+                ]
+            ],
+            "subject_level_PCA": false,
+            "subject_level_num_PC": 120
+        },
+        "cache": {}
+        }
+
+    Returns
+    -------
+    computation_output : dict
+        Example:
+        {
+        "output": {
+            "reduced_data": reduced_data,
+            "num_PC_global": num_PC_global,
+            "computation_phase": 'local_1'
+        },
+        "cache": {}
+        }
+
+    """
 
     state = args['state']
     inputs = args['input']
@@ -44,7 +94,10 @@ def local_1(args):
                                 for file in file_list]
             row_mean_file_type = inputs['mean_values'][1][0]
             # Read local row mean files
-            row_mean = ut.read_data(row_mean_file_list, row_mean_file_type, 'row_mean_global', state['clientId'])['0']
+            row_mean = ut.read_data(row_mean_file_list,
+                                    row_mean_file_type,
+                                    'row_mean_global',
+                                    state['clientId'])['0']
         else:
             row_mean = None
 
@@ -71,7 +124,7 @@ def local_1(args):
 
 
     # Save local projection and backprojection matrices
-
+    ######## Option currently not supported.
 
     # Compile results to be transmitted to remote and cached for reuse in next iteration
     computation_output = {
@@ -87,16 +140,17 @@ def local_1(args):
     return computation_output
 
 
-# if __name__ == '__main__':
+if __name__ == '__main__':
 
-#     parsed_args = json.loads(sys.stdin.read())
-#     phase_key = list(ut.listRecursive(parsed_args, 'computation_phase'))
+    parsed_args = json.loads(sys.stdin.read())
+    phase_key = list(ut.listRecursive(parsed_args, 'computation_phase'))
     
-#     if not phase_key:
-#         computation_output = local_1(parsed_args)
-#         # Transmit results to remote
-#         # as file (for large volumes of data, OS overhead):
-#         # as JSON string (for smaller volumes of data, JSON conversion overhead):
-#         sys.stdout.write(json.dumps(computation_output))
-#     else:
-#         raise ValueError("Error occurred at Local")
+    if not phase_key:
+        computation_output = local_1(parsed_args)
+        # Transmit results to remote
+        # as file (for large volumes of data, OS overhead):
+        
+        # as JSON string (for smaller volumes of data, JSON conversion overhead):
+        sys.stdout.write(json.dumps(computation_output))
+    else:
+        raise ValueError("Error occurred at Local")
